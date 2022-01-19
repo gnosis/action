8551764ea15a2bb4cb559dca9ae615105435c8da
Mikhail <mmvsha73@gmail.com>
2022-01-19 16:13:44 +0100

    update snapshots

--- src/run.ts
+++ src/run.ts
@@ -175,8 +175,7 @@ export async function runVersion({
 
   let prBodyPromise = (async () => {
     return (
-      `This PR was opened by the [Changesets release](https://github.com/changesets/action) GitHub action. When you're ready to do a release, you can merge this and publish to npm yourself or [setup this action to publish automatically](https://github.com/changesets/action#with-publishing)
-      }. If you're not ready to do a release yet, that's fine, whenever you add more changesets to ${branch}, this PR will be updated.
+      `This PR was opened by the [Changesets](https://github.com/gnosis/changesets-action-github-releases) GitHub action. When you're ready to do a release, you can merge this and publish to npm yourself. If you're not ready to do a release yet, that's fine, whenever you add more changesets to ${branch}, this PR will be updated.
       ${
         !!preState
           ? `
