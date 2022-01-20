d561d48dc9d53e9a953bbd8bb7931ac3fef0cdf5
Mikhail <mmvsha73@gmail.com>
2022-01-20 22:11:10 +0100

    trying to fix comment formatting

--- src/run.ts
+++ src/run.ts
@@ -183,8 +183,7 @@ export async function runVersion({
   let changedPackages = await getChangedPackages(cwd, versionsByDirectory);
 
   let prBodyPromise = (async () => {
-    return (
-      `This PR was opened by the [Changesets](https://github.com/gnosis/changesets-action-github-releases) GitHub action. When you're ready to do a release, you can merge this and publish to npm yourself. If you're not ready to do a release yet, that's fine, whenever you add more changesets to ${branch}, this PR will be updated.
+    return `This PR was opened by the [Changesets](https://github.com/gnosis/changesets-action-github-releases) GitHub action. When you're ready to do a release, you can merge this and publish to npm yourself. If you're not ready to do a release yet, that's fine, whenever you add more changesets to ${branch}, this PR will be updated.
       ${
         !!preState
           ? `
@@ -198,8 +197,8 @@ export async function runVersion({
       }
       
       # Releases
-      ` +
-      (
+      
+      ${(
         await Promise.all(
           changedPackages.map(async (pkg) => {
             let changelogContents = await fs.readFile(path.join(pkg.dir, "CHANGELOG.md"), "utf8");
@@ -216,8 +215,7 @@ export async function runVersion({
         .filter((x) => x)
         .sort(sortTheThings)
         .map((x) => x.content)
-        .join("\n ")
-    );
+        .join("\n ")}`;
   })();
 
   const finalPrTitle = `${prTitle}${!!preState ? ` (${preState.tag})` : ""}`;
